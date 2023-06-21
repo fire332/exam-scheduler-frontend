482d224ae0fc9142015904b25a501a438aa12676
fire332 <96039230+fire332@users.noreply.github.com>
2023-06-20 20:21:02 -0700

    setup github codespaces (#10)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { env } from 'node:process';
+import { defineConfig, type UserConfigExport } from 'vite';
+
+const config = {
+  plugins: [react()],
+  server: {
+    host: env.CODESPACES ? true : undefined,
+    proxy: {
+      '/api/': {
+        target: 'https://exam-scheduler-backend.onrender.com/',
+        changeOrigin: true
+      },
+      '/oauth/v2/': {
+        target: 'https://fancy-runnable-unicorn-dfmpp0.zitadel.cloud/oauth/v2/',
+        changeOrigin: true
+      }
+    }
+  }
+} satisfies UserConfigExport;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(config);
