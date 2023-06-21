35b6bda595ff47b3f30c2598f71a49886aebe0e3
fire332 <96039230+fire332@users.noreply.github.com>
2023-06-20 19:33:17 -0700

    dev url rewrites part 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api/': {
+        target: 'https://exam-scheduler-backend.onrender.com/',
+        changeOrigin: true
+      },
+      '/oauth/v2/': {
+        target: 'https://fancy-runnable-unicorn-dfmpp0.zitadel.cloud/oauth/v2/',
+        changeOrigin: true
+      }
+    }
+  }
+});
