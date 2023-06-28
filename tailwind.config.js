209826a003e23fa58d7235da8d49f195a8d54e4d
fire332 <96039230+fire332@users.noreply.github.com>
2023-06-28 02:52:42 -0700

    improve theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,17 +10,17 @@ const config = {
     extend: {
       colors: {
         primary: {
-          50: 'color(display-p3 0.933 0.98 0.902 / <alpha-value>)',
-          100: 'color(display-p3 0.878 0.961 0.82 / <alpha-value>)',
-          200: 'color(display-p3 0.757 0.922 0.639 / <alpha-value>)',
-          300: 'color(display-p3 0.635 0.882 0.459 / <alpha-value>)',
-          400: 'color(display-p3 0.514 0.843 0.278 / <alpha-value>)',
+          50: 'color(display-p3 0.929 1 0.878 / <alpha-value>)',
+          100: 'color(display-p3 0.875 0.992 0.788 / <alpha-value>)',
+          200: 'color(display-p3 0.749 0.969 0.592 / <alpha-value>)',
+          300: 'color(display-p3 0.627 0.929 0.412 / <alpha-value>)',
+          400: 'color(display-p3 0.506 0.882 0.239 / <alpha-value>)',
           500: 'color(display-p3 0.4 0.737 0.161 / <alpha-value>)',
-          600: 'color(display-p3 0.322 0.592 0.129 / <alpha-value>)',
-          700: 'color(display-p3 0.239 0.443 0.098 / <alpha-value>)',
-          800: 'color(display-p3 0.161 0.294 0.063 / <alpha-value>)',
-          900: 'color(display-p3 0.078 0.149 0.031 / <alpha-value>)',
-          950: 'color(display-p3 0.035 0.067 0.016 / <alpha-value>)'
+          600: 'color(display-p3 0.318 0.624 0.098 / <alpha-value>)',
+          700: 'color(display-p3 0.235 0.482 0.059 / <alpha-value>)',
+          800: 'color(display-p3 0.153 0.333 0.027 / <alpha-value>)',
+          900: 'color(display-p3 0.075 0.173 0.008 / <alpha-value>)',
+          950: 'color(display-p3 0.031 0.078 0 / <alpha-value>)'
         },
         secondary: {
           50: 'color(display-p3 0.98 0.898 0.906 / <alpha-value>)',
