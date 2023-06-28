b4c5552129f813bc79d9aee0ed19d61fa49450a4
stokely <stokelydiamantis@gmail.com>
2023-06-28 16:21:54 -0700

    fix type error

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,12 +2,7 @@ interface Props {
   icon?: React.ReactNode;
 }
 
-export default function Button({
-  children,
-  // TODO
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  icon
-}: React.PropsWithChildren<Props>) {
+export default function Button({ children }: React.PropsWithChildren<Props>) {
   return (
     <button className="h-16 w-full min-w-[6rem] rounded-md border-b-2 border-primary-600 bg-primary-500 px-8 text-xl text-white transition-all hover:border-2 hover:border-primary-500 hover:bg-transparent hover:text-primary-500">
       {children}
