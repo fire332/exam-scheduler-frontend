150981de351749d47ad7c85ce3bfe98ec7b8ae7e
fire332 <96039230+fire332@users.noreply.github.com>
2023-06-28 12:03:28 -0700

    fix lint errors

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import './index.css';
 
 import { CacheProvider, DevToolsManager } from '@rest-hooks/react';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CacheProvider
       managers={[...CacheProvider.defaultProps.managers, new DevToolsManager()]}
