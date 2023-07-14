9144e8ba8f5e122283556aef6d2c24b0e656062b
fire332 <96039230+fire332@users.noreply.github.com>
2023-07-14 01:05:57 -0700

    remove duplicate eslint extend

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,7 +14,6 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'plugin:react-hooks/recommended',
     'plugin:storybook/recommended',
     'prettier',
   ],
