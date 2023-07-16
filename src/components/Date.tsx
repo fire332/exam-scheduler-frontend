7a58b8009c7b97b41f84dfcd65717ebc98e3ae30
NDrew6071 <andrewsr.lam@gmail.com>
2023-07-15 21:49:15 -0700

    update Date to handle undefined type

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -1,9 +1,16 @@
 import { SlashIcon } from '@radix-ui/react-icons';
 import { DateTime } from 'luxon';
 
+function dtToNumbers(dt: DateTime): [string, string, string] {
+  return dt.toLocaleString(DateTime.DATE_SHORT).split('/') as unknown as [
+    string,
+    string,
+    string,
+  ];
+}
+
 export default function Date({ luxonDate }: { luxonDate: DateTime }) {
-  const dateString = luxonDate.toLocaleString(DateTime.DATE_SHORT);
-  const dateFields = dateString.split('/');
+  const dateFields = dtToNumbers(luxonDate);
   return (
     <div className="flex items-center text-sm">
       {dateFields[0].padStart(2, '0')}
