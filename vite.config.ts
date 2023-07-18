eeda4774e8ed11fea87c6c4136216d558bc28eee
fire332 <96039230+fire332@users.noreply.github.com>
2023-07-18 16:37:33 -0700

    update deps & fix vite tscfg paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
 import { env } from 'node:process';
 import { defineConfig, type UserConfigExport } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const config = {
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     host: env.CODESPACES ? true : undefined,
-    proxy: {
-      '/api/': {
-        target: 'https://fic-exam-scheduler-api-6f324588b682.herokuapp.com/',
-        rewrite: (path) => path.replace(/^\/api\//, ''),
-      },
-      '/auth/': {
-        target: 'https://fancy-runnable-unicorn-dfmpp0.zitadel.cloud/',
-        rewrite: (path) => path.replace(/^\/auth\//, ''),
-      },
-    },
   },
 } satisfies UserConfigExport;
 
