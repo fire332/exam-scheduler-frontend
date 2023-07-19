4ba52fd0c0a6f33f46bd18f4027bd153e5f4ac90
NDrew6071 <andrewsr.lam@gmail.com>
2023-07-18 21:01:09 -0700

    refactor OrderedDateInputItem to reduce duplicate code: DateWithCalendar

--- src/components/OrderedDateInputItem.tsx
+++ src/components/OrderedDateInputItem.tsx
@@ -1,10 +1,6 @@
-import {
-  CalendarIcon,
-  DragHandleDots2Icon,
-  Pencil1Icon,
-} from '@radix-ui/react-icons';
+import { DragHandleDots2Icon, Pencil1Icon } from '@radix-ui/react-icons';
 import type { DateTime } from 'luxon';
-import Date from './Date';
+import DateWithCalendar from './DateWithCalendar';
 
 export default function OrderedDate({ luxonDate }: { luxonDate: DateTime }) {
   return (
@@ -12,9 +8,8 @@ export default function OrderedDate({ luxonDate }: { luxonDate: DateTime }) {
       <div className="inline-flex">
         <DragHandleDots2Icon />
       </div>
-      <div className="inline-flex">
-        <CalendarIcon />
-        <Date luxonDate={luxonDate} />
+      <div className="inline-flex items-center justify-center">
+        <DateWithCalendar luxonDate={luxonDate} />
       </div>
       <div className="inline-flex">
         <Pencil1Icon />
