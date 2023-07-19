8ef1788a3e2f4188fb5a28d14b91cc4079966292
NDrew6071 <andrewsr.lam@gmail.com>
2023-07-18 22:59:46 -0700

    update OrderDateInputItem to better match Figma

--- src/components/OrderedDateInputItem.tsx
+++ src/components/OrderedDateInputItem.tsx
@@ -4,15 +4,17 @@ import DateWithCalendar from './DateWithCalendar';
 
 export default function OrderedDate({ luxonDate }: { luxonDate: DateTime }) {
   return (
-    <div className="w-px318 h-px50 mx-3 justify-between rounded bg-gray-50 shadow">
-      <div className="inline-flex">
-        <DragHandleDots2Icon />
-      </div>
-      <div className="inline-flex items-center justify-center">
-        <DateWithCalendar luxonDate={luxonDate} />
-      </div>
-      <div className="inline-flex">
-        <Pencil1Icon />
+    <div className="h-12 w-80 justify-between px-3">
+      <div className="inline-flex rounded bg-gray-50 shadow">
+        <div className="inline-flex">
+          <DragHandleDots2Icon />
+        </div>
+        <div className="mx-1 w-full">
+          <DateWithCalendar luxonDate={luxonDate} />
+        </div>
+        <div className="inline-flex">
+          <Pencil1Icon className="h-5 w-5" />
+        </div>
       </div>
     </div>
   );
