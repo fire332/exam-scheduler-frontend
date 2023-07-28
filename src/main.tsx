07005a7224a655405f98ef90520eba8c1ff73f23
stokely <stokelydiamantis@gmail.com>
2023-07-27 19:16:14 -0700

    add router devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -10,6 +11,18 @@ import type { ValueOf } from 'utility';
 import { authConfig } from './authConfig';
 import { router } from './routing';
 
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider {...authConfig}>
@@ -20,6 +33,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         ]}
       >
         <RouterProvider router={router} />
+        <TanStackRouterDevtools
+          initialIsOpen={false}
+          router={router}
+          position="bottom-right"
+        />
       </CacheProvider>
     </AuthProvider>
   </React.StrictMode>,
