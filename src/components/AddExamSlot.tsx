2e7c26dc21a2131a07d99714bccf8e122a8fbb65
stokely <stokelydiamantis@gmail.com>
2023-08-01 20:16:48 -0700

    finish AddExamSlot, add ComboChipInput

--- src/components/AddExamSlot.tsx
+++ src/components/AddExamSlot.tsx
@@ -5,16 +5,24 @@
 // TODO: refactor to use separate Date and Time inputs (better match Figma)
 // TODO: rename? -> ScheduleExam.tsx?
 
-import { CalendarIcon, InputIcon } from '@radix-ui/react-icons';
+import {
+  CalendarIcon,
+  ChevronLeftIcon,
+  InputIcon,
+} from '@radix-ui/react-icons';
+import { Link } from '@tanstack/router';
 import type { ScheduledExam, ScheduledExamLike } from 'api/ScheduledExam';
+import { DateTime } from 'luxon';
 import type { FormEventHandler } from 'react';
 import { useCallback } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
 import { FormProvider, useForm } from 'react-hook-form';
+import { scheduledExamsRoute } from 'routes/scheduled-exams/routing';
 import Button from './Button';
+import ComboChipInput from './FormInput/ComboChipInput';
 import DateTimeInput from './FormInput/DateTimeInput';
+import DurationInput from './FormInput/DurationInput';
 import TextInput from './FormInput/TextInput';
-import PageHeader from './PageHeader';
 
 interface Props {
   scheduledExam?: ScheduledExam;
@@ -22,31 +30,32 @@ interface Props {
 }
 
 export default function AddExamSlot({ scheduledExam, onSubmit }: Props) {
-  const defaultValues = Object.assign(
+  const defaultValues: ScheduledExamLike = Object.assign(
     {
       // courseCode: 'CMPT276',
-      startDateTime: '2023-09-07T15:30:00-0800',
-      isoDuration: 'PT1H35M',
+      // startDateTime: '2023-09-07T15:30:00-0800',
+      // isoDuration: 'PT1H35M',
       // instructorId: '219195704583651585', // < - Default
       // instructorId: '221523240743338241', // <- Admin Andy
       instructorId: '221523781573673217', // <- Instructor Ivy
       // instructorId: undefined,
-      locations: [{ roomName: 'DIS1 2020' }, { roomName: 'DIS1 2040' }],
+      // locations: [{ roomName: 'DIS1 2020' }, { roomName: 'DIS1 2040' }],
       proctorsRequested: [
         // {
         //   proctorId: '219195704583651585',
         // },
       ],
       proctorsConfirmed: [],
+      locations: [],
     },
     scheduledExam,
   );
-  // defaultValues.isoDuration =
-  // scheduledExam?.duration.shiftTo('minutes').minutes.toString() ?? '60';
+  defaultValues.isoDuration =
+    scheduledExam?.duration.shiftTo('minutes').minutes.toString() ?? '60';
 
-  // defaultValues.isoDatePrefs =
-  //   examRequest?.datePreferences.map((date) => date.toFormat('yyyy-LL-dd')) ??
-  //   [];
+  defaultValues.startDateTime = scheduledExam
+    ? DateTime.fromISO(scheduledExam.startDateTime).toISO()!.slice(0, 16)
+    : undefined;
 
   const methods = useForm<ScheduledExamLike>({
     defaultValues,
@@ -63,7 +72,17 @@ export default function AddExamSlot({ scheduledExam, onSubmit }: Props) {
     <FormProvider {...methods}>
       <form onSubmit={handleSubmit}>
         <div className="inline-flex h-full w-full flex-col">
-          <PageHeader>Add exam slot</PageHeader>
+          <header className="inline-flex h-20 w-full flex-row items-center text-xl font-bold">
+            <div>
+              <Link
+                className="inline-flex w-8 items-center justify-center"
+                to={scheduledExamsRoute.fullPath}
+              >
+                <ChevronLeftIcon width="24" height="24" />
+              </Link>
+            </div>
+            <div className="inline-flex">Add exam slot</div>
+          </header>
 
           <div className="inline-flex h-24 w-full flex-row gap-x-9">
             <TextInput
@@ -71,12 +90,7 @@ export default function AddExamSlot({ scheduledExam, onSubmit }: Props) {
               inputName="courseCode"
               labelIcon={InputIcon}
             />
-            {/* <div className="inline-flex w-full"> */}
-            {/* <TextInput labelText="Room" inputName="location.0" /> */}
-            {/* </div> */}
-          </div>
 
-          <div className="inline-flex h-24 w-full flex-row gap-x-9">
             <DateTimeInput
               labelText="Date and Start Time"
               inputName="startDateTime"
@@ -85,13 +99,11 @@ export default function AddExamSlot({ scheduledExam, onSubmit }: Props) {
           </div>
 
           <div className="inline-flex h-24 w-full flex-row gap-x-9">
-            {/* <DurationInput
+            <DurationInput
               labelText="Duration (minutes)"
               inputName="isoDuration"
               stepValue={15}
             />
-
-            <TextInput labelText="Instructor ID" inputName="instructorId" /> */}
 
             {/* <NumericInput
               labelText="Proctors"
@@ -100,22 +112,24 @@ export default function AddExamSlot({ scheduledExam, onSubmit }: Props) {
             /> */}
           </div>
 
-          {/* <div className="inline-flex h-24 w-full flex-row gap-x-9">
-            <TextInput
-              labelText="Proctors Requested"
-              inputName="proctorsRequested.0"
-            />
-            <TextInput
-              labelText="Proctors Confirmed"
-              inputName="proctorsConfirmed.0"
-            />
-          </div> */}
+          <div className="inline-flex h-auto w-full flex-row gap-x-9">
+            <ComboChipInput labelText="Room" inputName="locations" />
+          </div>
 
-          <div className="inline-flex w-full flex-row-reverse">
+          {/* <TextInput labelText="Instructor ID" inputName="instructorId" /> */}
+
+          <div className="inline-flex w-full">
             <div className="inline-flex h-9 w-40">
               <Button>Add Exam</Button>
             </div>
           </div>
+
+          {/* <div className="inline-flex w-full">
+            <div className="inline-flex h-9 w-40">
+              <Link>
+              </Link>
+            </div>
+          </div> */}
         </div>
       </form>
     </FormProvider>
