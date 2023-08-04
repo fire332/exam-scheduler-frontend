c57c6a9bd3cfad80ee8c054473acc8d4ce3d4105
fire332 <96039230+fire332@users.noreply.github.com>
2023-08-04 01:54:11 -0600

    fix button border animation

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,15 +1,37 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Component from './Button';
+import { ButtonIntent } from './constants';
 
 type Story = StoryObj<typeof Component>;
 
 const meta = {
   component: Component,
-  argTypes: { children: { control: 'text' } },
+  args: {
+    children: 'Click me',
+  },
+  argTypes: {
+    intent: { control: 'inline-radio', options: ['solid', 'outline', 'tonal'] },
+    children: { control: 'text' },
+    onClick: { action: 'onClick' },
+  },
 } satisfies Meta<typeof Component>;
 
 export default meta;
 
-export const Button: Story = {
-  render: (args) => <Component {...args}>{args.children}</Component>,
+export const Solid: Story = {
+  args: {
+    intent: ButtonIntent.Solid,
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    intent: ButtonIntent.Outline,
+  },
+};
+
+export const Tonal: Story = {
+  args: {
+    intent: ButtonIntent.Tonal,
+  },
 };
